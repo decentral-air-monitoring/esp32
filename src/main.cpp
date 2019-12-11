bfdb640d0d0249bec57e9b04d2a755a838a69b19
Marvin Gaube <dev@marvingaube.de>
2019-12-11 21:02:58 +0100

    Add WiFi Connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
 #include <Arduino.h>
+// ESP32-specific
+#include <WiFi.h>
+// Board-specific
+#include "heltec.h"
+// Peripherie
+
+// Data
+
+char ssid[] = "ffm.freifunk.net"; // ToDo: Dynamic configurable
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, true /*Serial Enable*/);
+  // Connect to WiFi
+  Heltec.display->clear();
+  Heltec.display->drawString(0, 0, "Connecting with");
+  Heltec.display->drawString(0, 10, ssid);
+  Heltec.display->display();
+  Serial.printf("Connect with %s\n",ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid);
+  // Wait for connection
+  while(!WiFi.isConnected()); // ToDo: Rebuild with events
+  // Print out success
+  Heltec.display->clear();
+  Heltec.display->drawString(0,0,"Connected!");
+  Heltec.display->drawString(0,10,WiFi.localIP().toString());
+  Heltec.display->display();
+  Serial.println("Connected!");
+  Serial.println(WiFi.localIP().toString());
+  // ToDo: PubSubClient
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
