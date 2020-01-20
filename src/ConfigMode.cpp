faa4367bab44ddf617958c30b11ef6d36013570f
Marvin Gaube <dev@marvingaube.de>
2020-01-20 19:47:36 +0100

    Prepare the use of serial configuration (broken)

--- src/ConfigMode.cpp
+++ src/ConfigMode.cpp
@@ -7,10 +7,28 @@ ConfigMode::ConfigMode() {
 void ConfigMode::init() {
     this->server.on("/", this->formular);
     this->server.begin(80);
+    Serial.println("Config Mode via Serial available:");
+    Serial.println("Enter \"key=value\"");
 }
 
 void ConfigMode::handle() {
     this->server.handleClient();
+    if(Serial.available()) {
+        Serial.readBytesUntil('\n', serialBuffer, SERIAL_BUF_LENGTH);
+        this->serialReceive();
+    }
+}
+// ToDo: sscanf-logic currently broken
+void ConfigMode::serialReceive() {
+    char key[20] = "";
+    char value[20] = "";
+    char dummy[1];
+    sscanf(this->serialBuffer,"%s%[=]%s",key,dummy,value);
+    if(preferences.putString(key,value)) { // ToDo: Don't use preferences directly, but use an interface to a config class
+        Serial.printf("Set %s to %s\n", key, value);
+    } else {
+        Serial.println("Error setting preferences!");
+    }
 }
 
 WebServer::THandlerFunction ConfigMode::formular() {
