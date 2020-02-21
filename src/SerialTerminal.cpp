cf2ac03e5620bf6048a8d5b2b9535cdf1bd73fea
Marvin Gaube <dev@marvingaube.de>
2020-02-21 11:52:18 +0100

    Move Serial Stuff from config mode to SerialTerminal

--- src/SerialTerminal.cpp
+++ src/SerialTerminal.cpp
@@ -1,5 +1,32 @@
 #include "SerialTerminal.hpp"
 
 SerialTerminal::SerialTerminal() {
-    
+
+}
+
+void SerialTerminal::init() {
+    this->printHelp();
+}
+
+void SerialTerminal::printHelp() {
+    Serial.println("Enter \"key=value\"");
+}
+
+void SerialTerminal::handle() {
+    if(Serial.available()) {
+        Serial.readBytesUntil('\n', serialBuffer, SERIAL_BUF_LENGTH);
+        this->receive();
+    }
+}
+
+void SerialTerminal::receive() {
+    char key[20] = "";
+    char value[20] = "";
+    char dummy[1];
+    sscanf(this->serialBuffer,"%s%[=]%s",key,dummy,value);
+    /*if(preferences.putString(key,value)) { // ToDo: Don't use preferences directly, but use an interface to a config class
+        Serial.printf("Set %s to %s\n", key, value);
+    } else {
+        Serial.println("Error setting preferences!");
+    }*/
 }
--- src/SerialTerminal.hpp
+++ src/SerialTerminal.hpp
@@ -3,10 +3,17 @@
 
 #include "common.hpp"
 
+#define SERIAL_BUF_LENGTH 20
+
 class SerialTerminal {
     public:
         SerialTerminal();
+        void init();
+        void handle();
+        void printHelp();
     private:
+        void receive();
+        char serialBuffer[SERIAL_BUF_LENGTH];
 };
 
 #endif
