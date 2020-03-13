246e95fb189cb51b736096162c010becd203df9b
Marvin Gaube <dev@marvingaube.de>
2020-03-13 13:18:15 +0100

    BME680: Implement Asynchronous Read, closes #11

--- src/airsensor/BME680.cpp
+++ src/airsensor/BME680.cpp
@@ -24,24 +24,34 @@ BME680::~BME680()
 
 void BME680::handle()
 {
+    if(this->bme->endReading()) {
+        this->status=true;
+        this->data.humidity = this->bme->readHumidity()*1000;
+        this->data.pressure = this->bme->readPressure();
+        this->data.temperature = this->bme->readTemperature()*1000;
+    }
 }
 
 airSensorData BME680::getData()
 {
-    if (! bme->performReading()) {
-        Serial.println("Failed to perform reading :(");
-        return data;
+    if(this->status) {
+    } else {
+        this->data.temperature = -30000;
+        this->data.humidity = -30000;
+        this->data.pressure = -30000;
     }
-    data.humidity = bme->readHumidity()*1000;
-    data.pressure = bme->readPressure();
-    data.temperature = bme->readTemperature()*1000;
     return data;
 }
 
 void BME680::startMeasurement() {
-
+    this->status = false;
+    this->endTime = this->bme->beginReading();
+    if(this->endTime == 0) {
+        Serial.println(F("BME680: Failure to start reading"));
+        return;
+    }
 }
 
 boolean BME680::measurementStatus() {
-    return true; 
+    return this->status; 
 }
--- src/airsensor/BME680.hpp
+++ src/airsensor/BME680.hpp
@@ -10,6 +10,8 @@ class BME680 : public AirSensor
 private:
     Adafruit_BME680* bme;
     airSensorData data;
+    unsigned long endTime;
+    boolean status = false;
 public:
     BME680();
     ~BME680();
